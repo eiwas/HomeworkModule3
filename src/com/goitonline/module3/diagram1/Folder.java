22e6534c2e576f3ca04d35988fc824e502a1d271
eiwas <m.khomulenko@gmail.com>
2016-03-11 19:32:50 +0300

    Initial commit

---
+++ src/com/goitonline/module3/diagram1/Folder.java
@@ -0,0 +1,10 @@
+package com.goitonline.module3.diagram1;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 11.03.2016.
+ */
+public class Folder {
+    List<File> files;
+}
---
+++ src/com/goitonline/module3/diagram1/SoundFile.java
@@ -0,0 +1,7 @@
+package com.goitonline.module3.diagram1;
+
+/**
+ * Created by Michael on 11.03.2016.
+ */
+public class SoundFile extends File {
+}
