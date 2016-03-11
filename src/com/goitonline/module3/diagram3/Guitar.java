3690770a6b5a7033ce1493f586460f825cc34c49
eiwas <m.khomulenko@gmail.com>
2016-03-11 20:05:47 +0300

    Initial commit

---
+++ src/com/goitonline/module3/diagram3/Guitar.java
@@ -0,0 +1,7 @@
+package com.goitonline.module3.diagram3;
+
+/**
+ * Created by Michael on 11.03.2016.
+ */
+public class Guitar extends MusicalInstrument {
+}
---
+++ src/com/goitonline/module3/diagram3/MusicShop.java
@@ -0,0 +1,10 @@
+package com.goitonline.module3.diagram3;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 11.03.2016.
+ */
+public class MusicShop {
+    List<MusicalInstrument> instruments;
+}
